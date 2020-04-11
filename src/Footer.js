09b71dc76a6d6d7dfce2348b709b218bebfa9b9d
gunishmatta <33680363+gunishmatta@users.noreply.github.com>
2020-04-11 18:31:04 +0530

    Commit 1
    Added png directory

--- src/Footer.js
+++ src/Footer.js
@@ -6,12 +6,13 @@ import "./styles.css";
 export default function Footer() {
   return (
     <div className="Footer">
-
 <h1>REACH OUT TO US ON.</h1>
-      <div class="reach-links">
-     
-      </div> 
- 
+      <div className="reach-links">
+        <a href="https://github.com/OpenSouceCode" > </a>
+<a href= "https://join.slack.com/t/opensourcecode/shared_invite/zt-dcirfg6f-IYydr1qUsAAYaq_ZA48jmg">
+</a>
+      </div>
+   
     </div>
   );
 }
