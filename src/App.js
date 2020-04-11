bfdcca63664b6968817ebbbd6a9ce7b9fd663505
gunishmatta <33680363+gunishmatta@users.noreply.github.com>
2020-04-11 17:50:58 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles.css";
+import WelcomeComponent from "./WelcomeComponent";
+import Footer from "./Footer";
+
+export default function App() {
+  return (
+    <div className="App">
+    <WelcomeComponent />
+    <Footer />
+    
+    </div>
+  );
+}
---
+++ src/Footer.js
@@ -0,0 +1,17 @@
+
+import React from "react";
+import "./styles.css";
+
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+
+<h1>REACH OUT TO US ON.</h1>
+      <div class="reach-links">
+     
+      </div> 
+ 
+    </div>
+  );
+}
