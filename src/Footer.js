1ee357cdd7f2676a82f3bfad7364ec8013cc363e
gunishmatta <33680363+gunishmatta@users.noreply.github.com>
2020-04-12 13:01:56 +0530

    Commit 3
    Style changes in footer

--- src/Footer.js
+++ src/Footer.js
@@ -8,9 +8,23 @@ export default function Footer() {
     <div className="Footer">
 <h1>REACH OUT TO US ON.</h1>
       <div className="reach-links">
-        <a href="https://github.com/OpenSouceCode" > </a>
-<a href= "https://join.slack.com/t/opensourcecode/shared_invite/zt-dcirfg6f-IYydr1qUsAAYaq_ZA48jmg">
-</a>
+
+
+      <a href="https://github.com/OpenSouceCode" >
+            <img alt="footer-github.png" src={require('./png/footer-github.png')} />
+        </a>
+        <a href="https://join.slack.com/t/opensourcecode/shared_invite/zt-dcirfg6f-IYydr1qUsAAYaq_ZA48jmg">
+          <img alt="footer-slack.png" src={require('./png/footer-slack.png')} />
+          </a>
+        
+        <a href="https://www.linkedin.com"> <img alt="footer-linkedin.png" src={require('./png/footer-linkedin.png')} />
+        </a>
+
+
+
+
+
+      
       </div>
    
     </div>
